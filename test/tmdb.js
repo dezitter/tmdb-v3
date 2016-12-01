580da5473bec4d24f4d7d262b10f3f25c490d823
Laurent Dezitter <laurent.dezitter@gmail.com>
2016-12-01 10:48:00 +0100

    implement searchMovie()

--- test/tmdb.js
+++ test/tmdb.js
@@ -1,7 +1,16 @@
+/* global beforeEach, context, describe, it */
+
 const Tmdb = require('../lib');
 const expect = require('chai').expect;
+const nock = require('nock');
+
+nock.disableNetConnect();
+
+const apiMock  = nock('https://api.themoviedb.org/3');
 
 describe('Tmdb', () => {
+    const apiKey = 'XXX';
+
     describe('#constructor()', () => {
         context('when no api key is given', () => {
 
@@ -10,6 +19,33 @@ describe('Tmdb', () => {
                     .to.throw('Missing api key');
             });
 
+        });
+    });
+
+    describe('#searchMovie()', () => {
+        const tmdb = new Tmdb({ apiKey });
+
+        beforeEach(() => {
+            apiMock.get('/search/movie')
+                   .query({ api_key: apiKey, query: 'foo' })
+                   .reply(200, '{ "page": 1, "results": []}');
+        });
+
+        context('when no query is given', () => {
+            it('throws an error', function() {
+                expect(() => tmdb.searchMovie())
+                    .to.throw('Missing query');
+            });
+        });
+
+        it('returns a thenable', function() {
+            expect(tmdb.searchMovie('foo'))
+                .to.respondTo('then');
+        });
+
+        it('parses the response', function() {
+            return tmdb.searchMovie('foo')
+                .then(response => expect(response).to.be.an('object'));
         });
     });
 });
